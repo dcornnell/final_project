7ed95f9cb2f754c0b5dbc86afc8f152218a366a9
Daniel Cornnell <daniel.cornnell@gmail.com>
2014-06-11 08:42:04 -0400

    trying to implement rating

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140610210309) do
+ActiveRecord::Schema.define(version: 20140611123012) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -53,6 +53,17 @@ ActiveRecord::Schema.define(version: 20140610210309) do
     t.integer  "score"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "rating"
+    t.integer  "rating_count"
+  end
+
+  create_table "routes_tags", id: false, force: true do |t|
+    t.integer "route_id"
+    t.integer "tag_id"
+  end
+
+  create_table "tags", force: true do |t|
+    t.string "name"
   end
 
   create_table "users", force: true do |t|
