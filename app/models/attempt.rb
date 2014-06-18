573e5d3c4b008320f4eee1c70a23ff260bbdcb31
Daniel Cornnell <daniel.cornnell@gmail.com>
2014-06-18 12:25:08 -0400

    more front end changes

--- app/models/attempt.rb
+++ app/models/attempt.rb
@@ -4,9 +4,13 @@ belongs_to :route
 validates_presence_of :date_attempted
 
 scope :this_day, -> {where('date_attempted = ?' , Date.today)}
+scope :yesterday, -> {where('date_attempted = ?', Date.today - 1)}
 scope :this_week, -> {where('date_attempted > ?', 7.days.ago)}
-scope :this_month, -> {where('date_attempted > ? ',   30.days.ago)}
-scope :this_year, -> {where('date_attempted > ? ',   365.days.ago)}
+scope :last_week, -> {where('date_attempted >= ? AND date_attempted <= ?', 14.days.ago, 7.days.ago)}
+scope :this_month, -> {where('date_attempted > ? ',   1.months.ago)}
+scope :last_month, -> {where('date_attempted >= ? AND date_attempted <= ?', 2.months.ago, 1.months.ago )}
+scope :this_year, -> {where('date_attempted > ? ',   1.years.ago)}
+scope :last_year, -> {where('date_attempted >= ? AND date_attempted  <= ?', 2.years.ago,   1.years.ago)}
 
 	# adds or subtracts points based on params provided
 	def add_modifiers(grade, completed, flash)
