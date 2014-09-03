7ca47afdee2c52605dc91dad7bd13342f4735cf4
Daniel Cornnell <daniel.cornnell@gmail.com>
2014-09-03 14:11:34 -0400

    trying to add paperclip stuff

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,7 +10,7 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_devise_permitted_parameters
-    registration_params = [:user_name, :email, :password, :password_confirmation]
+    registration_params = [:first_name, :last_name, :avatar, :description, :user_name, :email, :password, :password_confirmation]
 
     if params[:action] == 'update'
       devise_parameter_sanitizer.for(:account_update) { 
